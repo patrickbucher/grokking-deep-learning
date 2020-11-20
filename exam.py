0dc8cc8b8642b505448ea8cdd530d7397250363a
Patrick Bucher <patrick.bucher@mailbox.org>
2020-11-20 23:37:44 +0100

    made up example: exam

---
+++ exam.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+iq = np.array([98, 101, 93, 110, 95, 120, 89, 97, 127, 82])
+lessons = np.array([27, 34, 37, 12, 40, 25, 38, 25, 12, 20])
+studied = np.array([10, 12, 25, 32, 7, 33, 15, 5, 20, 60])
+
+# real weights:  [0.5, 0.2, 0.3]
+score = iq * 0.5 + lessons * 0.2 + studied * 0.3
+print(score)
+
+weights = np.array([1/3, 1/3, 1/3])
+alpha = 0.00001
+
+for i in range(10000):
+    for j in range(len(score)):
+        input = np.array([iq[j], lessons[j], studied[j]])
+        goal = score[j]
+
+        prediction = input.dot(weights)
+
+        delta = prediction - goal
+        error = delta ** 2
+        weight_delta = input * delta
+
+        weights -= weight_delta * alpha
+
+print(weights)
