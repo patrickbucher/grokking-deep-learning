f3116e3f2aa9cadb73316854e40478749a811ce1
Patrick Bucher <patrick.bucher@mailbox.org>
2020-11-21 10:49:04 +0100

    improved example

--- interest_rate.py
+++ interest_rate.py
@@ -5,19 +5,18 @@ import numpy as np
 investments = np.array([11_324.92, 29_876.21, 54_271.28, 101_987.9, 1_207_674.2])
 returns = investments * 1.0037  # 0.37% return (to be figured out)
 
-alpha = 1e-15
 weight = 1.00  # initial guess: no yield
+alpha = 1e-15
 
 finished = False
-for i in range(1000000):
+for i in range(1_000_000):
     for j in range(len(investments)):
         goal = returns[j]
         input = investments[j]
         prediction = input * weight
         delta = goal - prediction
-        error = delta ** 2
-        directed_error = delta * input
-        adjustment = directed_error * alpha
+        weight_delta = delta * input
+        adjustment = weight_delta * alpha
         if abs(adjustment) < 1e-18:
             finished = True
             break
